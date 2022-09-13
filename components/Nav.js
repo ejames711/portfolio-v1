5d27625509417148240b4b5d63328d834e438388
TrikKH <ejames711@gmail.com>
2022-09-13 13:22:01 -0500

    update terminal visuals and add placeholder for about/projects

--- components/Nav.js
+++ components/Nav.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Nav() {
     return (
-        <div className="flex flex-col gap-12 md:gap-24 items-center justify-center text-white text-4xl md:text-8xl font-roboto">
+        <div className="flex flex-col gap-10 md:gap-24 items-center justify-center text-white text-4xl md:text-8xl font-roboto">
             <Link href="/" ><a>HOME</a></Link>
             <Link href="/projects" ><a >PROJECTS</a></Link>
             <Link href="/about" ><a>ABOUT</a></Link>
