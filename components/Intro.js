4b638d2d80ff70bb63d84d2e8f1d0d332d079420
TrikKH <ejames711@gmail.com>
2022-09-12 21:37:02 -0500

    add round corners on intro

--- components/Intro.js
+++ components/Intro.js
@@ -18,7 +18,7 @@ const fadeIn = {
 
 export default function Intro() {
     return(
-        <motion.div className='flex flex-col md:flex-row items-center justify-evenly text-center md:text-justify h-1/2 md:h-2/3 w-3/4 md:w-screen lg:w-5/6 bg-zinc-900' initial="hidden" animate="visible" variants={fadeIn}>
+        <motion.div className='flex flex-col md:flex-row items-center justify-evenly rounded-lg text-center md:text-justify h-1/2 md:h-2/3 w-3/4 md:w-screen lg:w-5/6 bg-zinc-900' initial="hidden" animate="visible" variants={fadeIn}>
             <div className='flex flex-col gap-4 justify-center text-white'>
                 <h1 className='text-4xl md:text-8xl font-ubuntu' >Ethan Palas</h1>
                 <h2 className='text-2xl md:text-6xl xl:text-7xl font-sans' >Freelance Developer</h2>
