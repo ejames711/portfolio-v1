135f65a9cacb34511b7509445aa5c96dfdafd5e4
TrikKH <ejames711@gmail.com>
2022-09-13 13:35:10 -0500

    fix apos build error

--- pages/about.js
+++ pages/about.js
@@ -5,8 +5,8 @@ export default function About(){
             <video className={"object-fill absolute w-full h-full"} autoPlay muted loop src='/assets/video.mp4' type="video/mp4" />
             <div className="flex flex-col justify-center items-center z-10 w-2/3 gap-8 bg-black opacity-80 rounded-lg p-10">
                 <p className="text-3xl">
-                    I'm a self taught developer with interests specifically in blockchain, network automation, and game development. 
-                    I am currently self employed, doing freelance development for multiple DAO's.
+                    I&apos;m a self taught developer with interests specifically in blockchain, network automation, and game development. 
+                    I am currently self employed, doing freelance development for multiple DAOs.
                 </p>
                 <h2 className="text-4xl">Technologies I work with:</h2>
                 <ul className="text-4xl">
