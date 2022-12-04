1cb4d8ae23ebc95afe513566d009511804343c6f
TrikKH <ejames711@gmail.com>
2022-12-04 16:34:42 -0600

    restyle header

--- components/About.js
+++ components/About.js
@@ -1,13 +1,22 @@
 import Link from "next/link"
+import Image from "next/image"
 import Slideshow from "./Slideshow.js"
 
 export default function About() {
     return(
-        <div id="about" className="flex flex-col items-center justify-center w-screen h-screen mt-40 text-center text-white bg-black font-ubuntu" >
-            <div className="z-10 flex flex-col items-center justify-center gap-2 p-10 rounded-lg lg:w-2/3 md:gap-8 opacity-80">
+        <div id="about" className="flex flex-col items-center justify-center w-screen h-screen gap-20 mt-32 text-center text-white bg-black font-ubuntu" >
+            <div className="flex gap-8">
+                <Link href={'https://bitbucket.org/ethan-palas/'}>
+                    <Image src={"/assets/img/bitbucket.svg"} height={50} width={50} className='hover:cursor-pointer' />
+                </Link>
+                <Link href={'https://www.linkedin.com/in/ethan-palas-b9b3a417a/'}>
+                    <Image src={'/assets/img/linkedin.svg'} height={50} width={50} className='hover:cursor-pointer' />
+                </Link>
+            </div>
+            <div className="z-10 flex flex-col items-center justify-center gap-20 p-10 rounded-lg lg:w-2/3 opacity-80">
                 <p className="sm:text-xl md:text-3xl">
-                    Hello! My name is Ethan Palas. I&apos;m a self taught developer. My interests and hobbies include dApps, web development, and network automation. 
-                    I am currently self employed, doing freelance development for multiple DAOs.
+                    Hello! My name is Ethan Palas. I&apos;m a self taught developer with interests in dApps, web development, and game development. 
+                    I&apos;m currently self employed, doing freelance work for multiple DAOs.
                 </p>
                 <h2 className="sm:text-2xl md:text-4xl">Technologies I currently work with:</h2>
                 <Slideshow />
--- components/Intro.js
+++ components/Intro.js
@@ -20,13 +20,11 @@ const fadeIn = {
 export default function Intro() {
     return(
         <motion.div id='intro' className='flex flex-col items-center w-3/4 gap-4 mt-20 text-center justify-evenly md:flex-row' initial="hidden" animate="visible" variants={fadeIn}>
-            <div className='flex flex-col justify-center w-full h-full text-white md:gap-4'>
+            <div className='flex flex-col justify-center text-white md:gap-4'>
                 <h1 className='text-4xl md:text-8xl font-ubuntu' >Ethan Palas</h1>
                 <h2 className='font-sans text-2xl md:ml-20 md:text-5xl' >Freelance Developer</h2>
             </div>
-            <motion.div className='flex items-center justify-center w-full h-full' initial={{y: -10}} animate={{y: 10}} transition={{type: 'spring', stiffness: 10, repeat: Infinity, repeatType: 'reverse'}}>
-              <img src='/assets/img/eth.svg' className='w-full h-full'/>
-            </motion.div>
+            <Image src={'/assets/computer-white.svg'} width={250} height={250}/>
         </motion.div>
     )
 }
